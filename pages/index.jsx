ebd2edfebe8fc92a926360a527c8007e0879c97a
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 18:04:57 +0300

    docs added

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,9 +11,12 @@ export default function Home({ meetups }) {
 }
 
 /**
- * This is the code which only runs on the server.
- * During the build process, Next.js will generate a static version of the page.
- * It will provide the data that it needs to render the page.
+ * This is the code which only runs on the server side.
+ * It only runs once during the build process and Next.js will generate a static
+ * version of the page. It will provide the data that it needs to render the page.
+ *
+ * This is very good for SEO and performance.
+ * And our Meetup data barely changes so it's a good idea to cache it.
  */
 export async function getStaticProps() {
     const meetups = await getMeetups();
@@ -27,3 +30,19 @@ export async function getStaticProps() {
         revalidate: 10, // In seconds
     };
 }
+
+// /**
+//  * This is an alternative to getStaticProps which will be called on EVERY request.
+//  * Here you have access to the context object which contains the request.
+//  *
+//  * But here we don't use this method, because Meetups data is not changing frequently.
+//  * So it doesn't make sense to re-generate the page every time.
+//  */
+// export async function getServerSideProps({ req }) {
+//     const meetups = await getMeetups();
+
+//     // this props will be passed to the page component as props
+//     return {
+//         props: { meetups },
+//     };
+// }
