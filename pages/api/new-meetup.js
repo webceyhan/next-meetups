ebbdb1caf6c0ab02b23376f3085ff2a3167c2268
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 20:39:18 +0300

    create-meetup moved to lib

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,15 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const url = process.env.FIREBASE_URL;
-
-const createMeetup = async (meetup) => {
-    const response = await fetch(`${url}.json`, {
-        method: 'POST',
-        body: JSON.stringify(meetup),
-    });
-
-    return response.json();
-};
+import { createMeetup } from '../../lib/create-meetups';
 
 export default async function handler(req, res) {
     // Process a POST request
