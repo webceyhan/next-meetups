4496ca8c97fe0c11e01d35fc76241fe1fc3ee9e4
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 17:49:43 +0300

    static props logic added

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,26 +2,25 @@ import { useEffect, useState } from 'react';
 import { getMeetups } from '../api';
 import { MeetupList } from '../components/MeetupList';
 
-export default function Home() {
-    const [loading, setLoading] = useState(true);
-    const [meetups, setMeetups] = useState([]);
-
-    // note: without useEffect component will be re-rendered
-    // on every change of meetups which will cause the infinite loop
-    useEffect(() => {
-        getMeetups()
-            .then(setMeetups)
-            .then(() => setLoading(false));
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
+export default function Home({ meetups }) {
     return (
         <div>
             <h1>All Meetups</h1>
             <MeetupList meetups={meetups} />
         </div>
     );
+}
+
+/**
+ * This is the code which only runs on the server.
+ * During the build process, Next.js will generate a static version of the page.
+ * It will provide the data that it needs to render the page.
+ */
+export async function getStaticProps() {
+    const meetups = await getMeetups();
+
+    // this props will be passed to the page component as props
+    return {
+        props: { meetups },
+    };
 }
