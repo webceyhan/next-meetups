e3c199aa35888ca149c4f3342b07674382e1dfca
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 20:59:09 +0300

    favorites context provider hooked

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import { Layout } from '../components/Layout/Layout';
+import { FavoritesContextProvider } from '../store/FavoritesContext';
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <FavoritesContextProvider>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </FavoritesContextProvider>
     );
 }
