960a32f162fef95b76fc4c8125a1d558abb477a7
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 20:20:49 +0300

    new-meetup logic moved into api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { createMeetup } from '../../api';
+const url = process.env.FIREBASE_URL;
+
+const createMeetup = async (meetup) => {
+    const response = await fetch(`${url}.json`, {
+        method: 'POST',
+        body: JSON.stringify(meetup),
+    });
+
+    return response.json();
+};
 
 export default async function handler(req, res) {
     // Process a POST request
