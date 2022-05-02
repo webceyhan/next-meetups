bc3eaf28d278216e92d7a5fd3a203953189e8a06
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 20:02:52 +0300

    new-meetup creation moved to api folder

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { createMeetup } from '../../api';
+
+export default async function handler(req, res) {
+    // Process a POST request
+    if (req.method === 'POST') {
+        // insert the meetup into the database
+        await createMeetup(req.body);
+
+        // return a 201 (created) status code
+        return res.status(201).json({
+            message: 'Meetup created',
+        });
+    }
+}
--- pages/new-meetup.jsx
+++ pages/new-meetup.jsx
@@ -1,12 +1,22 @@
 import { useRouter } from 'next/router';
-import { createMeetup } from '../api';
 import { MeetupForm } from '../components/MeetupForm';
 
 export default function NewMeetup() {
     const router = useRouter();
 
-    const addMeetupHandler = (formData) => {
-        createMeetup(formData).then(() => router.push('/'));
+    const addMeetupHandler = async (formData) => {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+        });
+
+        // redirect if successful
+        if (response.status === 201) {
+            router.push('/');
+        }
+
+        // TODO: handle error
     };
 
     return (
