9c61738d015462f8c6f1b6cc2f6baa5092cf7d71
Evren Ceyhan <evren@ceyhan.io>
2022-05-02 14:22:59 +0300

    all meetups merged into index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getMeetups } from '../api';
+import { MeetupList } from '../components/MeetupList';
+
 export default function Home() {
+    const [loading, setLoading] = useState(true);
+    const [meetups, setMeetups] = useState([]);
+
+    // note: without useEffect component will be re-rendered
+    // on every change of meetups which will cause the infinite loop
+    useEffect(() => {
+        getMeetups()
+            .then(setMeetups)
+            .then(() => setLoading(false));
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
-            <h1 className="display-5">Hello Next.js</h1>
+            <h1>All Meetups</h1>
+            <MeetupList meetups={meetups} />
         </div>
     );
 }
